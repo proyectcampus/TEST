1c3d36c9537c9e181f983812ae4f9cf863d2227c
miguel <mirope@campusaula.com>
2014-09-11 13:04:41 +0200

    añado respuesta

--- Muerete.java
+++ Muerete.java
@@ -2,6 +2,7 @@ public class Muerete{
 
   public static void main (String.args[]){
     System.out.println("MUERETE JESSICA");
+	System.out.println("No entiendo porque eres tan malo conmigo");
   }
 
 }
