3bc3e60a456b463e9eb2c22449c24ab51c32e615
The Master <jacruza@campusaula.com>
2014-09-11 13:02:27 +0200

    Jessica te odio

--- Muerete.java
+++ Muerete.java
@@ -2,6 +2,10 @@ public class Muerete{
 
   public static void main (String.args[]){
     System.out.println("MUERETE JESSICA");
+    System.out.println("MUERETE JESSICA");
+    System.out.println("MUERETE JESSICA");
+    System.out.println("MUERETE JESSICA");
+
   }
 
 }
