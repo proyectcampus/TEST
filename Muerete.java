f33261ac189db46f59da69ba00c11fa3d6bbf46d
miguel <mirope@campusaula.com>
2014-09-11 13:12:38 +0200

    commit 1

--- Muerete.java
+++ Muerete.java
@@ -3,6 +3,7 @@ public class Muerete{
   public static void main (String.args[]){
     System.out.println("MUERETE JESSICA");
 	System.out.println("No entiendo porque eres tan malo conmigo");
+	System.out.println("es como que no has forni..");
   }
 
 }
